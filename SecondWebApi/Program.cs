e131232cb79490d2af2645087dd7094ecabeec95
Andriy Borkovich <borkovich25andri@gmail.com>
2025-01-04 18:03:09 +0200

    chore: removed comments

--- SecondWebApi/Program.cs
+++ SecondWebApi/Program.cs
@@ -6,8 +6,6 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,7 +13,6 @@ builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
